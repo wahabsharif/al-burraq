bd50b8843afe713a061829ef7ac5c5d9bad98d3c
wahab <www.rex19@gmail.com>
2024-06-25 14:21:44 +0500

    Index: Hero Section

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
-import React, { Component } from "react";
+import React from "react";
 import Slider from "react-slick";
 import Home from "../../assets/img/21.png";
 import Link from "next/link";
-// import SliderArrows from "../common/SliderArrows";
 import SearchProperty from "./SearchProperty";
 
 const Hero = () => {
@@ -20,107 +19,45 @@ const Hero = () => {
     autoplay: false,
     arrows: false,
   };
+
   return (
-    <div id="hero" className="relative">
+    <div className="relative left-0 right-0 p-4 mx-auto rounded-lg sm:p-6 md:p-10 bg-slate-800 width-90">
       <Slider ref={slider} {...settings}>
         <div>
-          <div className="grid items-center grid-cols-12 p-12 bg-darkBg">
-            <div className="col-span-12 pl-8 sm:col-span-12 md:col-span-12 lg:col-span-6">
-              <div className="flex items-center gap-2 icon">
+          <div className="grid items-center grid-cols-1 p-4 rounded-lg lg:grid-cols-2 sm:p-6 md:p-12 bg-slate-800">
+            <div className="order-2 lg:order-1 ">
+              <div className="flex items-center order-1 gap-2 mt-4 icon lg:order-none">
                 <i className="fa-solid fa-house-chimney text-darkGold"></i>
-                <span className="text-sm slide-title animated">
+                <span className="text-sm slide-title text-lightGold animated">
                   Real Estate Agency
                 </span>
               </div>
-              <div className="my-8 font-semibold slick-title">
-                <h1>Find Your Dream House By Us</h1>
+              <div className="order-2 my-8 font-semibold slick-title lg:order-none">
+                <h1 className="text-3xl md:text-4xl">
+                  Find Your Dream House By Us
+                </h1>
               </div>
-              <div className="px-4 my-4 border-l-2 border-gray-300 slick-description text-slate-500">
-                <p className="pr-0 text-sm leading-8 md:pr-36 lg:pr-36">
+              <div className="order-4 px-4 my-4 border-l-2 border-gray-300 slick-description text-slate-500 lg:order-none">
+                <p className="pr-0 text-sm leading-8 text-lightGold md:pr-36 lg:pr-24">
                   Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                   Quis, voluptas? Lorem ipsum dolor sit.
                 </p>
               </div>
-              <div className="mt-16 slick-button">
+              <div className="order-5 mt-8 sm:mb-20 lg:mt-16 slick-button lg:order-none">
                 <Link
-                  href={"/contact"}
-                  className="p-3 text-white transition-all rounded hover:bg-slate-800 hover:cursor-pointer bg-darkGold"
+                  href="/contact"
+                  className="p-3 text-white transition-all rounded hover:bg-slate-700 hover:cursor-pointer bg-darkGold"
                 >
                   Make An Inquiry
                 </Link>
               </div>
             </div>
-            <div className="hidden col-span-12 mt-20 md:mt-20 lg:mt-0 sm:col-span-12 md:col-span-12 lg:col-span-6 md:block">
-              <Image src={Home} alt="Loading..." />
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className="grid items-center grid-cols-12 p-12 bg-slate-100">
-            <div className="hidden mb-16 lg:my-0 md:block sm:col-span-12 md:col-span-12 lg:col-span-6">
-              <Image src={Home} alt="Loading..." />
-            </div>
-            <div className="col-span-12 pr-8 sm:col-span-12 md:col-span-12 lg:col-span-6">
-              <div className="flex items-center justify-end gap-2 icon">
-                <i className="fa-solid fa-house-chimney text-darkGold"></i>
-                <span className="text-sm slide-title animated">
-                  Real Estate Agency
-                </span>
-              </div>
-              <div className="my-8 font-semibold slick-title text-end">
-                <h1>Find Your Dream House By Us</h1>
-              </div>
-              <div className="px-4 my-4 border-r-2 border-gray-300 slick-description text-slate-500">
-                <p className="pl-10 pr-0 text-sm leading-8 md:pl-36 lg:pl-36">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Quis, voluptas? Lorem ipsum dolor sit.
-                </p>
-              </div>
-              <div className="mt-16 slick-button text-end">
-                <Link
-                  href={"/contact"}
-                  className="p-3 text-white transition-all rounded hover:bg-slate-800 hover:cursor-pointer bg-darkGold"
-                >
-                  Make An Inquiry
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className="grid items-center grid-cols-12 p-12 bg-slate-100">
-            <div className="col-span-12 pl-8 sm:col-span-12 md:col-span-12 lg:col-span-6">
-              <div className="flex items-center gap-2 icon">
-                <i className="fa-solid fa-house-chimney text-darkGold"></i>
-                <span className="text-sm slide-title animated">
-                  Real Estate Agency
-                </span>
-              </div>
-              <div className="my-8 font-semibold slick-title">
-                <h1>Find Your Dream House By Us</h1>
-              </div>
-              <div className="px-4 my-4 border-l-2 border-gray-300 slick-description text-slate-500">
-                <p className="pr-0 text-sm leading-8 md:pr-36 lg:pr-36">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Quis, voluptas? Lorem ipsum dolor sit.
-                </p>
-              </div>
-              <div className="mt-16 slick-button">
-                <Link
-                  href={"/contact"}
-                  className="p-3 text-white transition-all rounded hover:bg-slate-800 hover:cursor-pointer bg-darkGold"
-                >
-                  Make An Inquiry
-                </Link>
-              </div>
-            </div>
-            <div className="hidden col-span-12 mt-20 md:mt-20 lg:mt-0 sm:col-span-12 md:col-span-12 lg:col-span-6 md:block">
-              <Image src={Home} alt="Loading..." />
+            <div className="order-1 mt-8 lg:order-2 lg:mt-0">
+              <Image src={Home} alt="Loading..." className="w-full h-auto" />
             </div>
           </div>
         </div>
       </Slider>
-      {/* <SliderArrows slider={slider} top="top-2/4" /> */}
       <SearchProperty />
     </div>
   );
